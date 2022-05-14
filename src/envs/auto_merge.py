735ba83c765c5fd8f8a536d1c9d9357f6bfd5636
Salar Arbabi <sa00443@surrey.ac.uk>
2022-05-14 02:54:02 +0100

    Remove methods dedicated to imagined env

--- src/envs/auto_merge.py
+++ src/envs/auto_merge.py
@@ -11,9 +11,6 @@ class EnvAutoMerge(EnvMerge):
         super().__init__()
         self.seed(2022)
 
-    def env_reward_reset(self):
-        self.is_large_deceleration = False
-
     def initialize_env(self, episode_id):
         """Initiates the environment
         """
@@ -50,27 +47,23 @@ class EnvAutoMerge(EnvMerge):
                                                        [self.dummy_stationary_car])
             actions = vehicle.act()
             joint_action.append(actions)
-            act_long = actions[0]
-            if act_long < -5:
-                self.is_large_deceleration = True
-
-            if self.time_step > 0:
-                vehicle.act_long_p = vehicle.act_long_c
-            vehicle.act_long_c = act_long
         return joint_action
 
     def get_sdv_action(self, decision):
         self.sdv.neighbours = self.sdv.my_neighbours(self.vehicles+[self.dummy_stationary_car])
         actions = self.sdv.act(decision)
-        act_long = actions[0]
-        if self.time_step > 0:
-            self.sdv.act_long_p = self.sdv.act_long_c
-        self.sdv.act_long_c = act_long
-
         return actions
 
+    def log_actions(self, vehicle, actions):
+        act_long = actions[0]
+        if vehicle.act_long_c:
+            vehicle.act_long_p = vehicle.act_long_c
+        vehicle.act_long_c = act_long
+
     def track_history(self, vehicle):
-        if vehicle.id == 2:
+        """Use history for prediction
+        """
+        if vehicle.id != 1:
             obs = vehicle.neur_observe()
             vehicle.update_obs_history(obs[0])
 
@@ -78,49 +71,16 @@ class EnvAutoMerge(EnvMerge):
         """ steps the environment forward in time.
         """
         assert self.vehicles, 'Environment not yet initialized'
-        # print(self.time_step, ' ########################### STEP ######')
         joint_action = self.get_joint_action()
         sdv_action = self.get_sdv_action(decision)
+        self.log_actions(self.sdv, sdv_action)
+        self.sdv.step(sdv_action)
 
         for vehicle, actions in zip(self.vehicles, joint_action):
+            self.log_actions(vehicle, actions)
             self.track_history(vehicle)
             vehicle.step(actions)
-
-        self.sdv.step(sdv_action)
         self.time_step += 1
-
-    def planner_observe(self):
-        """?
-        """
-        # return self.rng.randint(-20, 20)
-        return self.sdv.glob_x
-
-    def is_terminal(self):
-        """ Set conditions for instance at which the episode is over
-        Note: Collisions are not possible, since agent does not take actions that
-            result in collisions
-
-        Episode is complete if:
-        (1) agent successfully performs a merge
-        """
-        # return False
-        if self.sdv.is_merge_complete():
-            return True
-
-    def get_reward(self):
-        """
-        Reward is set to encourage the following behaviours:
-        1) perform merge successfully (TTM)
-        2) avoid reckless decisions
-        """
-        total_reward = 0
-        if self.sdv.is_merge_complete():
-            total_reward += 0.1
-        #
-        if self.is_large_deceleration:
-            total_reward -= 0.7
-
-        return total_reward
 
     def all_cars(self):
         """
