545686e896d454260b954a1a5dc43a177e610a20
Salar Arbabi <sa00443@surrey.ac.uk>
2022-05-14 20:32:51 +0100

    -

--- src/tree_search/imagined_env.py
+++ src/tree_search/imagined_env.py
@@ -10,6 +10,11 @@ class ImaginedEnv(EnvAutoMerge):
     def __init__(self):
         super().__init__()
 
+    def env_reward_reset(self):
+        """This is reset with every planner timestep
+        """
+        self.bad_action_in_env = False
+
     def copy_attrs(self, state):
         for attrname in ['vehicles', 'sdv', 'time_step']:
             attrvalue = getattr(state, attrname)
@@ -20,23 +25,63 @@ class ImaginedEnv(EnvAutoMerge):
         Sets sdv's prior belief about other drivers
         """
         for vehicle in self.vehicles:
-            print('before ', vehicle.driver_params['aggressiveness'])
-            vehicle.driver_params['aggressiveness'] = self.rng.uniform(0.1, 0.9)
+            vehicle.driver_params['aggressiveness'] = self.rng.uniform(0.01, 0.99)
             vehicle.set_driver_params()
-            print('after ', vehicle.driver_params['aggressiveness'])
+
+    def is_bad_action(self, vehicle, actions):
+        return vehicle.id != 1 and actions[0] < -5
 
     def step(self, joint_action, decision=None):
         """ steps the environment forward in time.
         """
         assert self.vehicles, 'Environment not yet initialized'
-        # print(self.time_step, ' ########################### STEP ######')
-        # joint_action = self.get_joint_action()
         sdv_action = self.get_sdv_action(decision)
 
         for vehicle, actions in zip(self.vehicles, joint_action):
-            self.track_history(vehicle, actions)
+            self.log_actions(vehicle, actions)
+            self.track_history(vehicle)
             vehicle.step(actions)
+            if self.is_bad_action(vehicle, actions):
+                self.bad_action_in_env = True
 
-        self.track_history(self.sdv, sdv_action)
+        self.log_actions(self.sdv, sdv_action)
         self.sdv.step(sdv_action)
         self.time_step += 1
+
+    def is_terminal(self):
+        """ Set conditions for instance at which the episode is over
+        Note: Collisions are not possible, since agent does not take actions that
+            result in collisions
+
+        Episode is complete if:
+        (1) agent successfully performs a merge
+        """
+        # return False
+        if self.sdv.is_merge_complete():
+            return True
+
+    def get_reward(self):
+        """
+        Reward is set to encourage the following behaviours:
+        1) perform merge successfully  
+        2) avoid reckless decisions
+        """
+        total_reward = 0
+        if self.sdv.is_merge_complete():
+            if not self.sdv.abort_been_chosen:
+                total_reward += 0.3
+            else:
+                total_reward += 0.1
+
+        if self.bad_action_in_env:
+            total_reward -= 0.6
+        return total_reward
+
+    def planner_observe(self):
+        x_road = 0
+        for vehicle in self.vehicles:
+            x_road += vehicle.glob_x
+        return x_road
+        # return self.rng.randint(-100, 100)
+        # return self.sdv.glob_x
+        # return self
